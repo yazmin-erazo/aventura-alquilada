c76cc8c4c98208866d44b3afa9e46a736f443ebc
Naty <nataliajmoreira@gmail.com>
2023-05-16 09:28:35 -0300

    Se arma página de detalles sin estilo

--- Web/digital-booking-c3-e-1/src/components/products/Details.jsx
+++ Web/digital-booking-c3-e-1/src/components/products/Details.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { useParams, Link  } from 'react-router-dom'
+import { useParams, Link, useNavigate  } from 'react-router-dom'
 
 const Details = () => {
 
     const params = useParams();
+    const navigate = useNavigate();
 
   return (
     <>
     <div className='detailsHeader'>
         <div>Producto</div>
-         <Link to="/"><i className="fa-solid fa-chevron-left"></i></Link> {/*tambien se puede usar useNavigate para volver para atrás */}
+        <Link to="/"><i className="fa-solid fa-chevron-left"></i></Link> {/*tambien se puede usar useNavigate para volver para atrás */}
+        <button onClick={() => navigate(-1)}><i className="fa-solid fa-chevron-left"></i></button>
     </div>
     <div>Details of {params.id}</div>
     </>
