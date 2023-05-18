f3718f6d4c23d36e4a875da8296deadae83b64fc
Lorena Sanchez Fernandez <lorraines690.ls@gmail.com>
2023-05-18 18:35:56 +0000

    Revert "Merge branch 'develop' into 'main'"
    This reverts merge request !9

--- API/digital-booking-c3-e-1/src/main/java/com/digitalbooking/digitalbooking/infrastructure/category/adapter/CategoryRepositoryMySql.java
+++
@@ -1,6 +0,0 @@
-package com.digitalbooking.digitalbooking.infrastructure.category.adapter;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepositoryMySql extends JpaRepository<CategoryEntity, Long > {
-}
