4c1e977a4ffb8c29ed3de62655a38f84be757ef9
BelenGuillamondegui <belenguilla1@gmail.com>
2023-05-25 17:44:20 -0300

    Se arma form de registro sin conexion al back

---
+++ Web/digital-booking-c3-e-1/src/components/user/userAddForm/RegisterUser.jsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from "react";
+import InputWithLabel from "../../common/input/InputWithLabel";
+import ButtonPrimary from "../../common/Buttons/ButtonPrimary";
+import styles from "./registerUser.module.css";
+
+const RegisterUser = () => {
+  const [user, setUser] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    checkPassword: "",
+  });
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    checkPassword: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const validateForm = () => {
+      let isValid = true;
+      const errors = {
+        name: "",
+        lastName: "",
+        email: "",
+        password: "",
+        checkPassword: "",
+      };
+      const nameRegex = /^[a-zA-ZáÁéÉíÍóÓúÚñÑüÜ'][a-zA-ZáÁéÉíÍóÓúÚñÑüÜ\s']*$/;
+
+      const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/;
+
+      if (!user.name) {
+        errors.name = "El nombre es obligatorio";
+        isValid = false;
+      } else if (user.name.trim() !== user.name) {
+        errors.name = "El nombre no debe contener espacios al principio";
+        isValid = false;
+      } else if (!nameRegex.test(user.name)) {
+        errors.name = "El nombre contiene caracteres no válidos";
+        isValid = false;
+      }
+
+      if (!user.lastName) {
+        errors.lastName = "El apellido es obligatorio";
+        isValid = false;
+      } else if (user.lastName.trim() !== user.lastName) {
+        errors.lastName = "El apellido no debe contener espacios al principio";
+        isValid = false;
+      } else if (!nameRegex.test(user.lastName)) {
+        errors.lastName = "El apellido contiene caracteres no válidos";
+        isValid = false;
+      }
+
+      if (!user.email) {
+        errors.email = "El email es obligatorio";
+        isValid = false;
+      } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+        errors.email = "El email no es válido";
+        isValid = false;
+      }
+
+      if (!user.password) {
+        errors.password = "La contraseña es obligatoria";
+        isValid = false;
+      } else if (user.password.trim() !== user.password) {
+        errors.password = "La contraseña no debe contener espacios";
+        isValid = false;
+      } else if (user.password.length < 6) {
+        errors.password = "La contraseña debe tener al menos 6 caracteres";
+        isValid = false;
+      } else if (!passwordRegex.test(user.password)) {
+        errors.password = "La contraseña debe contener letras y números";
+        isValid = false;
+      }
+
+      if (!user.checkPassword) {
+        errors.checkPassword = "Debes confirmar la contraseña";
+        isValid = false;
+      } else if (user.checkPassword.trim() !== user.checkPassword) {
+        errors.checkPassword =
+          "La contraseña no debe contener espacios al principio";
+        isValid = false;
+      } else if (user.password !== user.checkPassword) {
+        errors.checkPassword = "Las contraseñas no coinciden";
+        isValid = false;
+      }
+
+      setFormErrors(errors);
+      return isValid;
+    };
+    if (validateForm()) {
+      console.log("formulario enviado");
+    } else {
+      console.log("El formulario contiene errores");
+    }
+  };
+
+  return (
+    <div className= {styles.container}>
+      <img src="/registerUser.png" alt="" />
+      <form className={styles["form-container"]}onSubmit={handleSubmit}>
+        {/* <label className="form-label">Nombre</label>
+        <input className="form-input"
+          type="text"
+          onChange={(event) =>
+            setUser({ ...user, name: event.target.value })
+          }
+          
+        /> */}
+        <InputWithLabel type={"text"} onChange={setUser}>
+          Nombre
+        </InputWithLabel>
+
+        {/* {formErrors.name && <span className="form-error">{formErrors.name}</span>} */}
+        {/* <label className="form-label">Apellido</label>
+        <input className="form-input"
+        type="text"
+        onChange={(event) =>
+          setUser({ ...user, lastName: event.target.value })
+        }
+      /> */}
+        <InputWithLabel type={"text"} onChange={setUser}>
+          Apellido
+        </InputWithLabel>
+
+        {/* {formErrors.lastName && <span className="form-error">{formErrors.lastName}</span>}
+
+<label className="form-label">Email</label>
+<input className="form-input"
+type="email"
+onChange={(event) => setUser({ ...user, email: event.target.value })}
+/> */}
+        <InputWithLabel type={"email"} onChange={setUser}>
+          Email
+        </InputWithLabel>
+
+        {/* {formErrors.email && <span className="form-error">{formErrors.email}</span>}
+
+<label className="form-label">Contraseña</label>
+<input className="form-input"
+type="password"
+          onChange={(event) =>
+            setUser({ ...user, password: event.target.value })
+          }
+        /> */}
+        <InputWithLabel type={"password"} onChange={setUser}>
+          Contraseña
+        </InputWithLabel>
+
+        {/* {formErrors.password && <span className="form-error">{formErrors.password}</span>}
+        <label className="form-label">Verifique la contraseña</label>
+        <input className="form-input"
+        type="password"
+        onChange={(event) =>
+          setUser({ ...user, checkPassword: event.target.value })
+        }
+      /> */}
+        <InputWithLabel type={"password"} onChange={setUser}>
+          Confirmación de contraseña
+        </InputWithLabel>
+
+        {/* {formErrors.checkPassword && <span className="form-error">{formErrors.checkPassword}</span>} */}
+
+        <ButtonPrimary onClick={handleSubmit}>Enviar</ButtonPrimary>
+
+        {/* <button className="form-submit-button">Enviar</button> */}
+      </form>
+    </div>
+  );
+};
+
+export default RegisterUser;
--- Web/digital-booking-c3-e-1/src/main.jsx
+++ Web/digital-booking-c3-e-1/src/main.jsx
@@ -7,6 +7,7 @@ import AddProduct from './components/products/productAddForm/RegisterProduct.jsx
 import Crud from "./components/products/Crud/Crud.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterUser from "./components/user/userAddForm/RegisterUser.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -17,6 +18,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path='products/:id' element={<Details />} />
         <Route path='administration/add' element={<AddProduct />} />
         <Route path='administration/' element={<Crud />} />
+        <Route path='auth/register' element={<RegisterUser />} />
+
       </Route>
     </Routes>
     </React.StrictMode>
