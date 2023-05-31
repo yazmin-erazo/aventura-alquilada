874fee65ae633718ebb0800a7c33fe32d66b0f36
Yazmin Erazo <ylerazom@gmail.com>
2023-05-31 18:02:24 -0500

    Mejorando el contenedor del footer header y enlaces a las imagenes

--- Web/digital-booking-c3-e-1/src/components/common/Footer.jsx
+++ Web/digital-booking-c3-e-1/src/components/common/Footer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
+import { FiGitlab, FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
   return (
@@ -7,21 +8,31 @@ const Footer = () => {
         <div className="isologotipo">
           <img
             className="logo"
-            src="/isologotipo.svg"
+            src="https://c3-e1-digital-booking.s3.us-east-2.amazonaws.com/img/isologotipo.svg"
             alt="isologotipo de Digital Booking"
           />
-          <img className="lema" src="/lema.png" alt="lema de Digital Booking" />
-          <img
-            className="copyright"
-            src="/copyright.png"
-            alt="imagen copyright"
-          />
+          <p>Sin equipo no hay aventura</p>
+          <div className="copyright">
+            <AiOutlineCopyright />{" "}
+            <p>
+              2023<span>|</span> Copyright
+            </p>
+          </div>
         </div>
+
         <div className="footerIcons">
-          <img src="/icon facebook.png" alt="icono Facebook" />
-          <img src="/Vector.png" alt="icono Linkedin" />
-          <img src="/tweet.png" alt="icono Tweeter" />
-          <img src="/insta.png" alt="icono Instagram" />
+          <a
+            href="https://gitlab.ctd.academy/ctd/hispanos/proyecto-integrador-1/proyecto-integrador-0523/1021pt-c3/equipo-01"
+            className="icon-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiGitlab className="icon" />
+          </a>
+          <a href="#" className="icon-link">
+            {" "}
+            <FiInstagram class="icon" />
+          </a>
         </div>
       </div>
     </div>
--- Web/digital-booking-c3-e-1/src/components/common/Header.jsx
+++ Web/digital-booking-c3-e-1/src/components/common/Header.jsx
@@ -3,13 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/AuthContext";
 
 const Header = () => {
-
-  const {isLogedIn, user, dispatch} = useContext(UserContext);
+  const { isLogedIn, user, dispatch } = useContext(UserContext);
   const navigate = useNavigate();
   const logoutHandler = () => {
-    dispatch({type: "LOGOUT"})
-    navigate('/');
-  }
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
 
   console.log(user);
 
@@ -17,64 +16,71 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <Link to="/" className="noUnderlined">
-          <img src="/Digital Booking.svg" alt="logotipo Digital Booking" />
+          <img
+            src="https://c3-e1-digital-booking.s3.us-east-2.amazonaws.com/img/digital-booking.svg"
+            alt="logotipo Digital Booking"
+          />
           <h5 className="lemaHeader">Sin equipo no hay aventura</h5>
         </Link>
-        { isLogedIn ? <div>
-          <input type="checkbox" id="userData" />
-          <label className="userDataIcon" htmlFor="userData">
-            <div className="user-name">
-              {user.name + " " + user.lastname}
-            </div>
-            <div className="user-logo">
-              <div>
-                {user.name.slice(0, 1) + user.lastname.slice(0, 1)}
+        {isLogedIn ? (
+          <div>
+            <input type="checkbox" id="userData" />
+            <label className="userDataIcon" htmlFor="userData">
+              <div className="user-name">{user.name + " " + user.lastname}</div>
+              <div className="user-logo">
+                <div>{user.name.slice(0, 1) + user.lastname.slice(0, 1)}</div>
               </div>
-            </div>
-          </label>
-          <ul className="user-profile">
-            <li>Perfil</li>
-            <hr />
-            <li>Mis favoritos</li>
-            <hr />
-            <li>Configuración</li>
-            <li className="logout" onClick={logoutHandler}>Cerrar Sesión</li>
-          </ul>
-        </div> :
-        <div className="headerBtn">
-          <Link to="auth/register">
-          <button className="cuentaBtn">Crear cuenta</button>
-          </Link>
-          <Link to='login'>
-          <button className="sesionBtn">Iniciar sesión</button>
-          </Link>
-        </div> 
-        }
+            </label>
+            <ul className="user-profile">
+              <li>Perfil</li>
+              <hr />
+              <li>Mis favoritos</li>
+              <hr />
+              <li>Configuración</li>
+              <li className="logout" onClick={logoutHandler}>
+                Cerrar Sesión
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="headerBtn">
+            <Link to="auth/register">
+              <button className="cuentaBtn">Crear cuenta</button>
+            </Link>
+            <Link to="login">
+              <button className="sesionBtn">Iniciar sesión</button>
+            </Link>
+          </div>
+        )}
         <nav>
-          { isLogedIn ? <ul className="user-profile">
-            <li>Perfil</li>
-            <hr />
-            <li>Mis favoritos</li>
-            <hr />
-            <li>Configuración</li>
-            <li className="logout" onClick={logoutHandler}>Cerrar Sesión</li>
-          </ul> :
-          <>
-          <input type="checkbox" id="menu" />
-          <label className="menuIcon" htmlFor="menu">
-            {" "}
-            ☰{" "}
-          </label>
-          <ul>
-            <Link to ="auth/register"className="noUnderlined">
-            <li>Crear cuenta</li>
-            </Link>
-            <Link to='login'className="noUnderlined">
-            <li>Iniciar sesión</li>
-            </Link>
-          </ul>
-          </>
-          }
+          {isLogedIn ? (
+            <ul className="user-profile">
+              <li>Perfil</li>
+              <hr />
+              <li>Mis favoritos</li>
+              <hr />
+              <li>Configuración</li>
+              <li className="logout" onClick={logoutHandler}>
+                Cerrar Sesión
+              </li>
+            </ul>
+          ) : (
+            <>
+              <input type="checkbox" id="menu" />
+              <label className="menuIcon" htmlFor="menu">
+                {" "}
+                ☰{" "}
+              </label>
+              <ul>
+                <Link to="auth/register" className="noUnderlined">
+                  <li>Crear cuenta</li>
+                </Link>
+                <Link to="login" className="noUnderlined">
+                  <li>Iniciar sesión</li>
+                </Link>
+              </ul>
+            </>
+          )}
         </nav>
       </div>
     </div>
