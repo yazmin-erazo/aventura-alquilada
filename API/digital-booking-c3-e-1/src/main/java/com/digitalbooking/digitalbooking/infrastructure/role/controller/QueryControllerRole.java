f3b41d3dfcbfa035216531ebc79159169becd75e
Lorena Sanchez <lorraines690.ls@gmail.com>
2023-05-31 19:17:40 -0500

    create jacoco test and test unit

--- API/digital-booking-c3-e-1/src/main/java/com/digitalbooking/digitalbooking/infrastructure/role/controller/QueryControllerRole.java
+++ API/digital-booking-c3-e-1/src/main/java/com/digitalbooking/digitalbooking/infrastructure/role/controller/QueryControllerRole.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,12 +21,14 @@ public class QueryControllerRole {
     private RoleHandler roleHandler;
 
     @GetMapping("{id-role}")
+    @PreAuthorize("@authorizationFilter.hasPermission('roleList')")
     @Operation(summary = "find role", description = "Method to find role by Id")
     public ResponseEntity<RoleDTO> getRole(@PathVariable("id-role") Long id) {
         return ResponseEntity.ok(roleHandler.findById(id));
     }
 
     @GetMapping("roles")
+    @PreAuthorize("@authorizationFilter.hasPermission('roleList')")
     @Operation(summary = "list roles", description = "Method to fetch the roles")
     public ResponseEntity<List<RoleDTO>> listRoles(){
         return ResponseEntity.ok(roleHandler.getRoles());
