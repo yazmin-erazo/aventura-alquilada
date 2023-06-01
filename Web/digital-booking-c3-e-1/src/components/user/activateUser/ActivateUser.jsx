8df6f7c27062be5595b0b86b16a7728c6ce3f469
Lorena Sanchez <lorraines690.ls@gmail.com>
2023-06-01 15:48:55 -0500

    update ActivateUser

--- Web/digital-booking-c3-e-1/src/components/user/activateUser/ActivateUser.jsx
+++ Web/digital-booking-c3-e-1/src/components/user/activateUser/ActivateUser.jsx
@@ -22,7 +22,21 @@ const ActivateUser = () => {
 
   return (
     <>
-     { response && (response.status == 200 ? <div> <h1>¡Activación exitosa!</h1>    <p>Tu cuenta ha sido activada con éxito. Ahora puedes ingresar a <span>Digital</span> <span>Booking</span> y disfrutar de nuestros servicios.</p></div> : <div className={styles.card}> {( response.status == 500 ? "Ha ocurrido un error de servidor" : response.data.mensaje)} </div>)}
+     { response && (response.status === 200 ? (
+          <div style={{ backgroundColor: "#edf6ff", textAlign: "center", padding: "50px", fontFamily: "Arial, sans-serif" }}>
+            <p>Hola!</p>
+            <p>¡Tu cuenta ahora está disponible! Esperamos que disfrutes de nuestra plataforma y descubras todos los beneficios que tenemos para ti.</p>
+            <div>
+            </div>
+            <p>Si tienes algún problema para acceder a tu cuenta, por favor, no dudes en ponerte en contacto con nosotros.</p>
+            <p>Saludos,</p>
+            <p>El equipo de <span style={{ color: "#00008B" }}>Digital</span> <span style={{ color: "#008000" }}>Booking</span></p>
+          </div>
+        ) : (
+          <div className={styles.card}>
+            {response.status === 500 ? "Ha ocurrido un error de servidor" : response.data.mensaje}
+          </div>
+        ))}
     </>
   );
 };
