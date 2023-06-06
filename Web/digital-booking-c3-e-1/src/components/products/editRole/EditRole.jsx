98d6308f11c3bca87c773de5169ec5283350b8a5
Lorena Sanchez <lorraines690.ls@gmail.com>
2023-06-06 12:27:06 -0500

    update editRole and newRole

--- Web/digital-booking-c3-e-1/src/components/products/editRole/EditRole.jsx
+++ Web/digital-booking-c3-e-1/src/components/products/editRole/EditRole.jsx
@@ -2,15 +2,15 @@ import { useCallback, useEffect, useState } from "react";
 import TableRow from "../../common/Table/TableRow";
 import styles from "./EditRole.module.css";
 import ButtonPrimary from "../../common/Buttons/ButtonPrimary";
-import { Link, useNavigate, useNavigation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import RolesService from "../../../shared/services/RolesService";
+import Swal from 'sweetalert2';
 
 const Crud = () => {
   const [roles, setRoles] = useState([]);
   const navigate = useNavigate()
 
   useEffect(() => {
-    // Llamada a la API para obtener la lista de roles
     const fetchRoles = async () => {
       try {
         const response = await RolesService.getAll();
@@ -25,15 +25,30 @@ const Crud = () => {
   }, []);
 
   const handleDelete = useCallback(async (roleId) => {
-    
-    try {
-      // Lógica para eliminar el rol con la ID "roleId"
-      console.log("Rol eliminado con ID:", roleId);
-      const updatedRoles = roles.filter((role) => role.id !== roleId);
-      setRoles(updatedRoles);
-    } catch (error) {
-      console.log("Error al eliminar el rol:", error);
-    }
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás revertir esta acción!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Sí, bórralo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        try {
+          console.log("Rol eliminado con ID:", roleId);
+          const updatedRoles = roles.filter((role) => role.id !== roleId);
+          setRoles(updatedRoles);
+          Swal.fire(
+            '¡Eliminado!',
+            'El rol ha sido eliminado.',
+            'success'
+          )
+        } catch (error) {
+          console.log("Error al eliminar el rol:", error);
+        }
+      }
+    });
   }, [roles]);
 
   const handleEdit = useCallback((roleId) => {
