52833eba9d03915022aad85c9b3e990bab00f8b0
Yazmin Erazo <ylerazom@gmail.com>
2023-06-09 12:26:26 -0500

    Modificados estilo en Vista de detalles

--- Web/digital-booking-c3-e-1/src/components/resources/rating/RatingStats.jsx
+++ Web/digital-booking-c3-e-1/src/components/resources/rating/RatingStats.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./RatingStats.module.css";
 
-const RatingStats = () => {
+const RatingStats = ({ color, totalColor }) => {
   const [averageRating, setAverageRating] = useState(0);
   const [totalRatings, setTotalRatings] = useState(0);
 
@@ -11,8 +11,8 @@ const RatingStats = () => {
 
   const generateRandomStats = () => {
     // const randomAverage = Math.random() * 5;
-    const randomAverage = Math.random() * (5 - 4) + 4; 
-    const randomTotal = Math.floor(Math.random() * 100); 
+    const randomAverage = Math.random() * (5 - 4) + 4;
+    const randomTotal = Math.floor(Math.random() * 100);
     setAverageRating(randomAverage.toFixed(1));
     setTotalRatings(randomTotal);
   };
@@ -21,12 +21,14 @@ const RatingStats = () => {
     <div className={styles.ratingStats}>
       <div className={styles.ratingStatsItem}>
         <div className={styles.ratingStatsAverage}>
-          <span className={styles.ratingStatsIcon}>&#9733;</span>
-          <span className={styles.ratingStatsRating}>
+          <span className={styles.ratingStatsIcon} style={{ color }}>
+            &#9733;
+          </span>
+          <span className={styles.ratingStatsRating} style={{ color }}>
             {averageRating}
           </span>
         </div>
-        <div className={styles.ratingStatsTotal}>
+        <div className={styles.ratingStatsTotal} style={{ color: totalColor }}>
           {totalRatings} valoraciones
         </div>
       </div>
