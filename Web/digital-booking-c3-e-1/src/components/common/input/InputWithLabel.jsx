10b0b461e229138ecccc934b429ca61efdb98163
Yazmin Erazo <ylerazom@gmail.com>
2023-06-10 18:31:30 -0500

    Arreglados estilos de errores y placeholder

--- Web/digital-booking-c3-e-1/src/components/common/input/InputWithLabel.jsx
+++ Web/digital-booking-c3-e-1/src/components/common/input/InputWithLabel.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const InputWithLabel = ({ children, type, value, onChange, isEditable }) => {
+const InputWithLabel = ({ children, type, value, onChange, isEditable, placeholder, error }) => {
+  const hasError = error && error.length > 0;
+
   return (
-    <div className={styles.inputContainer}>
+    <div className={`${styles.inputContainer} ${hasError ? styles.error : ''}`}>
       <label className={styles.label}>{children}
-      <input
-        className={styles.input}
-        type={type}
-        value={value}
-        onChange={onChange}
-        placeholder="Ingresa el valor"
-        disabled={isEditable === undefined ? false: !isEditable}
-      />
+        <input
+          className={`${styles.input} ${hasError ? styles.inputError : ''}`}
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          disabled={isEditable === undefined ? false : !isEditable}
+        />
       </label>
+      {hasError && <p className={styles.errorMessage}>{error}</p>}
     </div>
   );
 };
