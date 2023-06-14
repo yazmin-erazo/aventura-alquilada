7cd9b9bbdd8ed564a8e998f6dd6d415c0cb93bff
Lorena Sanchez <lorraines690.ls@gmail.com>
2023-06-14 13:53:40 -0500

    update Qualification

--- Web/digital-booking-c3-e-1/src/components/resources/qualification/Qualification.jsx
+++ Web/digital-booking-c3-e-1/src/components/resources/qualification/Qualification.jsx
@@ -4,7 +4,6 @@ import Swal from "sweetalert2";
 import ProductsService from "../../../shared/services/ProductsService";
 import InputWithLabel from "../../common/input/InputWithLabel";
 
-
 const Qualification = ({ isLoggedIn, productId }) => {
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
@@ -27,39 +26,39 @@ const Qualification = ({ isLoggedIn, productId }) => {
     if (!isLoggedIn) {
       Swal.fire({
         title: "Debes iniciar sesión",
-        text: "¡Comparte tu opinión! Inicia sesión para calificar y dejar un comentario sobre este producto. Tu experiencia es valiosa para nosotros y para otros usuarios.",
+        text: "Inicia sesión para calificar y comentar sobre este producto",
         icon: "info",
       });
       return;
     }
-    if(!comment){
+    if (!comment) {
       Swal.fire({
         title: "Error",
         text: "El comentario es requerido para enviar la calificación",
         icon: "error",
       });
-    }else{
+    } else {
       const commentRequest = {
         productId,
         comment: comment,
-        score: starCount
+        score: starCount,
       };
-  
+
       try {
         await ProductsService.comment(commentRequest);
-  
+
         Swal.fire({
           title: "¡Calificación enviada!",
           text: `Has calificado el producto con ${starCount} estrellas y dejado el comentario: ${comment}`,
           icon: "success",
         });
-  
-        setComment("")
-  
+
+        setComment("");
       } catch (error) {
         Swal.fire({
           title: "Error",
-          text: "Hubo un problema enviando tu calificación. Por favor, inténtalo de nuevo más tarde",
+          text:
+            "Hubo un problema enviando tu calificación. Por favor, inténtalo de nuevo más tarde",
           icon: "error",
         });
       }
@@ -89,17 +88,20 @@ const Qualification = ({ isLoggedIn, productId }) => {
     <div className={styles.reviewComponent}>
       <h4>Valorar producto</h4>
       <div className={styles.starsContainer}>{renderStars()}</div>
-      <div className={styles.commentContainer}>
-        <InputWithLabel
-          type="text"
-          value={comment}
-          placeholder = "Escribe tu comentario..."
-          onChange={(event) =>
-            handleCommentChange(event)
-          }
-        >
-        </InputWithLabel>
-      </div>
+      {isLoggedIn ? (
+        <div className={styles.commentContainer}>
+          <InputWithLabel
+            type="text"
+            value={comment}
+            placeholder="Escribe tu comentario..."
+            onChange={(event) => handleCommentChange(event)}
+          />
+        </div>
+      ) : (
+        <p className={styles.loginMessage}>
+          ¡Comparte tu opinión! Inicia sesión para calificar y dejar un comentario sobre este producto. Tu experiencia es valiosa para nosotros y para otros usuarios.
+        </p>
+      )}
     </div>
   );
 };
