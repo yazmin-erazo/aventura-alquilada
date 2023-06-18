be01979732e2032c6f986a6af77304dc9e608126
Yazmin Erazo <ylerazom@gmail.com>
2023-06-18 14:34:22 -0500

    Localización del usuario

--- Web/digital-booking-c3-e-1/src/components/home/HomePage.jsx
+++ Web/digital-booking-c3-e-1/src/components/home/HomePage.jsx
@@ -3,22 +3,48 @@ import SearchEngine from "../search/SearchEngine/SearchEngine";
 import CategoryList from "./Category/CategoryList";
 import styles from "./HomePage.module.css";
 import RecommendedList from "./Recommended/RecommendedList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FilterSidebar from "../search/SearchEngine/FilterSidebar";
 
 const HomePage = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [searchParams, setSearchParams] = useState(null);
   const [filterParams, setFilterParams] = useState({});
+  const [userLocation, setUserLocation] = useState(null);
 
   const handleSearch = (params) => {
     setSearchParams(params);
     setFilterParams({});
-  }
+
+    //-------- START Ubicación del usuario ------
+    params.userLocation = userLocation;
+  };
 
   const handleFilterChange = (filters) => {
     setFilterParams(filters);
   };
+
+  const getCurrentLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error al obtener la localizacion del usuario:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation no soportada por el navegador");
+    }
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+  //--------  FIN Ubicación del usuario ------
+  console.log(userLocation);
 
   return (
     <>
@@ -49,13 +75,14 @@ const HomePage = () => {
             </div>
             <section className={styles.recommendedContainer}>
               <h3 className={styles.subtitle}>
-                {searchParams ? 'Resultados de su Búsqueda' : 'Recomendados'}
+                {searchParams ? "Resultados de su Búsqueda" : "Recomendados"}
               </h3>
               <div className={styles.productGrid}>
                 <RecommendedList
                   selectedCategory={selectedCategory}
                   searchParams={searchParams}
                   filterParams={filterParams}
+                  userLocation={userLocation}
                 />
               </div>
             </section>
