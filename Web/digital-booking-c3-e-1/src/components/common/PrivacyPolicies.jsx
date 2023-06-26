7e5e40db1cadbd1ac73c8528e063658b1303ab21
Lorena Sanchez <lorraines690.ls@gmail.com>
2023-06-26 00:27:00 -0500

    add privacy policies

---
+++ Web/digital-booking-c3-e-1/src/components/common/PrivacyPolicies.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import './PrivacyPolicies.css';
+
+const PrivacyPolicy = () => (
+    <div className="privacy-policy">
+        <h1>Política de Privacidad</h1>
+        <p>Digital Booking, titular del website <a
+            href="http://equipamiento-deportivo-static.s3-website.us-east-2.amazonaws.com/">http://equipamiento-deportivo-static.s3-website.us-east-2.amazonaws.com/</a>
+            (desde ahora, “website”) garantiza la protección de todos los datos de carácter personal que proporcione el usuario
+            en el mismo. En cumplimiento de lo establecido en la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de
+            Datos de Carácter Personal, así como lo dispuesto en y el Reglamento (UE) 2016/679 del Parlamento Europeo y del
+            Consejo.</p>
+
+        <h2>¿Quién es el responsable de tratamiento de sus datos?</h2>
+        <ul>
+            <li>Identidad: <span className="color-blue">DIGITAL</span> <span className="color-green">BOOKING</span></li>
+            <li>Dirección postal: 123 High Street, London, SW1A 1AA, Reino Unido.</li>
+            <li>Correo electrónico: digitalhouse.dh123@gmail.com</li>
+        </ul>
+
+        <h2>¿Con qué finalidades son recabadas los datos? ¿Por cuánto tiempo conservaremos sus datos?</h2>
+
+        <p>Atender, gestionar y resolver las consultas, preguntas, propuestas, petición de presupuestos y cualquier otra
+            información requerida por el interesado (formulario de contacto). El tiempo de conservación será mientras dure el
+            tratamiento para la gestión de las consultas.</p>
+
+        <p>Suscripción a la página para recibir las nuevas entradas en el blog. La suscripción se realiza mediante la
+            cumplimentación del formulario de newsletter. El tiempo de conservación será mientras dure el tratamiento, salvo que
+            el usuario ejerza sus derechos de cancelación antes.</p>
+
+        <p>Mediante el formulario de contacto se procede a la recogida y tratamiento automatizado de los datos personales como
+            consecuencia de la consulta, solicitud o suscripción de cualquier servicio/s de los ofrecidos por el propietario del
+            website .El tiempo de conservación será mientras dure el tratamiento, salvo que el usuario ejerza sus derechos de
+            cancelación antes.</p>
+
+        <p>Recogida y tratamiento automatizado de los datos personales, como consecuencia de la navegación por las páginas del
+            website (puede ver más abajo la política de cookies).</p>
+
+        <p>Envío por medios electrónicos de información de publicidad, noticias y novedades relacionadas con los productos y
+            servicios del website (formulario de suscripción a newsletter). El tiempo de conservación es mientras dure el
+            tratamiento, salvo que el usuario ejerza sus derechos de cancelación antes.</p>
+
+        <p>Relación comercial con el cliente. El tiempo de conservación será el necesario para el tratamiento. Contratos y
+            facturas cinco y diez años, respectivamente.</p>
+
+        <h2>¿Cuál es la base legal para el tratamiento de datos?</h2>
+
+        <p>El tratamiento de datos personales que lleva a cabo Digital Booking, a través de su website, tiene como base legal
+            el consentimiento.</p>
+
+        <p>Dicho consentimiento del interesado es expreso, libre, específico, informado e inequívoco.</p>
+
+        <p>En la recogida y el tratamiento de los datos de carácter personal se han adoptado las medidas de seguridad
+            adecuadas
+            para evitar la pérdida, el acceso no autorizado o la manipulación de los mismos, de acuerdo con lo establecido en el
+            Real Decreto 1720/2007, de 21 de diciembre.</p>
+
+        <p>DIGITAL BOOKING se compromete a proteger la información confidencial a la que tenga acceso.</p>
+
+        <h2>¿A qué destinatarios y funciones de Marketing comunicarán tus datos?</h2>
+
+        <p>Digital Booking se compromete a que, estos datos personales no serán compartidos o cedidos a terceros sin el
+            consentimiento expreso de los usuarios.</p>
+
+        <p>En ningún caso la transferencia de esos datos implicará un uso que exceda el consentimiento expresado a los fines y
+            condiciones presentes en esta política de privacidad.</p>
+
+        <p> En caso de facilitarnos datos de carácter personal de otras personas sin indicar este hecho a DIGITAL BOOKING, lo hace con su consentimiento expreso y habiendo sido informado, con carácter previo, de los extremos contenidos
+            en esta cláusula. Del mismo modo, rogamos nos comunique cualquier variación que pueda darse en los datos
+            facilitados.</p>
+
+        <p>DIGITAL BOOKING no se hace responsable de la cesión de datos de terceros que no hayan prestado su
+            consentimiento ni tengan conocimiento de los contenidos de la presente cláusula, siendo responsabilidad de la
+            persona
+            que cede dichos datos al titular del website.</p>
+
+        <p>En caso de no facilitar los datos requeridos, los servicios de la página web podrían no estar disponibles en su
+            totalidad. El hecho de no facilitar el correo electrónico, requisito para la suscripción a la newsletter, o su
+            incorrecta transcripción en el formulario, impediría realizar las comunicaciones que constituyen su finalidad (al no
+            poderlas recibir el usuario). De no facilitar los datos requeridos en el formulario de contacto (nombre, email,
+            asunto
+            y mensaje), no se podrían llevar a cabo de forma satisfactoria los servicios solicitados. La negación a aceptar la
+            política de cookies podría suponer la pérdida de funcionalidades de la página web.</p>
+
+        <p>El Usuario certifica que es mayor de 14 años y que por lo tanto posee la capacidad legal necesaria para la
+            prestación del consentimiento en cuenta al tratamiento de sus datos de carácter personal y todo ello, de conformidad
+            con lo establecido en la presente Política de Privacidad.</p>
+
+        <p>El Usuario cuenta, en todo momento, con la posibilidad de ejercitar los derechos de acceso, rectificación,
+            cancelación y oposición sobre sus datos personales así como el de revocación del consentimiento para cualquier de
+            las finalidades antes señaladas, enviando a DIGITAL BOOKING carta debidamente firmada a nuestra dirección postal,
+            arriba indicada, donde consten claramente los datos de contacto, a la cual deberá acompañarse fotocopia de su
+            DNI/NIF o documento que acredite su identidad.</p>
+
+        <h2>El consentimiento expreso manifestado al rellenar el formulario de inscripción, comprende también lo siguiente:
+        </h2>
+
+        <p>DIGITAL BOOKING se reserva el derecho a subcontratar a empleados para la prestación de servicios de diversa
+            índole en el para el conveniente desarrollo y organización de la actividad del website.</p>
+
+        <p>Del mismo modo, se reserva el derecho a contratar a terceros para el tratamiento y gestión de las cuestiones de
+            ámbito
+            legislativo en general y, de lo relativo a protección de datos en concreto. En todos estos casos, DIGITAL BOOKING obligará a la empresa contratada y/o profesional particular y a los encargados de la gestión del
+            tratamiento de los datos personales cedidos, a firmar un documento de confidencialidad de datos personales para
+            asegurar la privacidad de esta información.</p>
+
+        <p>El usuario consiente en que se cedan sus datos personales cuando así sea requerido por las Autoridades
+            administrativas
+            competentes o por mandato judicial.</p>
+
+        <p>El Usuario autoriza al tratamiento de los datos personales cedidos a DIGITAL BOOKING en los términos
+            establecidos en la presente cláusula.</p>
+    </div>
+);
+
+export default PrivacyPolicy;
